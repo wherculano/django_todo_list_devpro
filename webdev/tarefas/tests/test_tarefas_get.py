8018e46776b65f17cd5bd6b20a85ace72a95f3ad
Wagner Herculano <wagherculano@hotmail.com>
2024-03-18 22:45:00 -0300

    Added tests

--- webdev/tarefas/tests/test_tarefas_get.py
+++ webdev/tarefas/tests/test_tarefas_get.py
@@ -1,8 +1,47 @@
+from django.http import HttpResponse
 from django.test import Client
 from django.urls import reverse
+import pytest
+from pytest_django.asserts import assertContains
 
 
-def test_status_code(client: Client):
+@pytest.fixture
+def response(client: Client) -> HttpResponse:
+    """ Retorna resposta da página Home
+
+    Args:
+        client (Client): Classe Client de testes do Django
+
+    Returns:
+        HttpResponse: Resposta de acesso à pagina.
+    """
     # named url -> reverse(app:view)
-    response = client.get(reverse('tarefas:home'))
+    resp = client.get(reverse('tarefas:home'))
+    return resp
+
+
+def test_status_code(response: HttpResponse) -> None:
+    """ Verifica se o status code retornado é 200
+
+    Args:
+        response (HttpResponse): Resposta obtida pela página.
+    """
     assert response.status_code == 200
+
+
+def test_formulario_presente(response: HttpResponse) -> None:
+    """ Verifica se há o formulário presente na página.
+
+    Args:
+        response (HttpResponse): Resposta obtida pela página.
+    """
+    assertContains(response, '<form>')
+
+
+def test_botao_salvar_presente(response: HttpResponse) -> None:
+    """ Verifica se há o botão de submit na página.
+
+    Args:
+        response (HttpResponse): Resposta obtida pela página.
+    """
+    assertContains(response, '<button type="submit"')
