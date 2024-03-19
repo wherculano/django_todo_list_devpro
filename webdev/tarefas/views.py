75d02fa476da18639d13bfb0821d5f4b3de3080d
Wagner Herculano <wagherculano@hotmail.com>
2024-03-18 23:49:21 -0300

    Added fuction with method POST

--- webdev/tarefas/views.py
+++ webdev/tarefas/views.py
@@ -1,4 +1,16 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
+from webdev.tarefas.form import TarefaNovaForm
+
 
 def home(request):
+    if request.method == 'POST':
+        form = TarefaNovaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('tarefas:home'))
+        else:
+            return render(request, 'tarefas/home.html', {'form': form}, status=400)
     return render(request, 'tarefas/home.html')
